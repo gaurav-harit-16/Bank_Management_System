f1e2ca665d20e5ce6fc4528b5da6feaefdfa608e
Gaurav Harit <63534799+gaurav-harit-16@users.noreply.github.com>
2023-09-18 18:01:56 +0530

    Update ConnectionTosql.java

--- BankManagmentSystem/src/ConnectionTosql.java
+++ BankManagmentSystem/src/ConnectionTosql.java
@@ -6,7 +6,7 @@ public class ConnectionTosql {
     public ConnectionTosql(){
         try {
 //            Class.forName("com.mysql.cj.jdbc.Driver");
-            c= DriverManager.getConnection("jdbc:mysql:///bankManagementSystem", "root", "Gaurav#01");
+            c= DriverManager.getConnection("jdbc:mysql:///bankManagementSystem", "Your_UserName", "Your_Pasword");
             s= c.createStatement();
         }catch (Exception e){
             System.out.println(e);
